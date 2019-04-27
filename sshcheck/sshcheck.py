53a5100c903957a319bca43e5659113b8743535f
Devin Collins <me@imdevinc.com>
2019-04-26 22:35:38 -0600

    Fixed bad variable

--- sshcheck/sshcheck.py
+++ sshcheck/sshcheck.py
@@ -2,18 +2,19 @@ import requests
 import os
 import sys
 
+
 def getKeys(user):
     keys = []
     try:
         r = requests.get('https://api.github.com/users/{}/keys'.format(user))
         r.raise_for_status()
-    except ex:
+    except Exception as ex:
         print(ex)
         return keys
     for key in r.json():
         keys.append(key["key"])
     return keys
-    
+
 
 if __name__ == '__main__':
     user = os.getenv('GITHUB_USER')
@@ -22,12 +23,12 @@ if __name__ == '__main__':
         sys.exit(1)
 
     authorizedKeysFile = os.getenv('AUTHORIZED_HOSTS', './authorized_hosts')
-    
+
     keys = getKeys(user)
     if len(keys) == 0:
         print('No keys found')
         sys.exit(0)
-    
+
     with open(authorizedKeysFile, 'w') as file:
         for key in keys:
             file.write('{}\n'.format(key))
